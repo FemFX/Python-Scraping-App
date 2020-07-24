9eca753fea62ee984a6534bef98273ad04c4d447
FemFX <ignedko34@gmail.com>
2020-07-24 09:35:47 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+def save():
+    with open('parse_info.txt', 'a') as file:
+        file.write(f'{comp["title"]} -> Price : {comp["price"]} -> Link : {comp["link"]}\n')
+
+def parse():
+    URL = 'https://www.olx.kz/elektronika/'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+    }
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.findAll('div', class_="offer-wrapper")
+    comps = []
+    for item in items:
+        comps.append({
+            'title': item.find('a', class_='marginright5 link linkWithHash detailsLink').get_text(strip=True),
+            'price': item.find('p', class_='price').get_text(strip=True),
+            'link': item.find('a', class_='marginright5 link linkWithHash detailsLink').get('href')
+        })
+        global comp
+        for comp in comps:
+            print(f'{comp["title"]} -> Price : {comp["price"]} -> Link : {comp["link"]}')
+            save()
+
+parse()
